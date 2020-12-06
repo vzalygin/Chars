0e074d847a56abaa1615855f35a36f4c0961addb
Muver <via.zyn@ya.ru>
2020-12-06 20:05:31 +0300

    new num_encoding file and class

---
+++ Python/num_encoding.py
@@ -0,0 +1,10 @@
+class NumEncoding:
+    @classmethod
+    def encoding(cls, s):
+        """Преобразует исходную строку в строку чисел в десятичной системе счисления"""
+        return 0
+
+    @classmethod
+    def decoding(cls, n):
+        """Преобразует исходную строку из чисел в нормальный вид"""
+        return 'a'
