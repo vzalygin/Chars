7c938d810a5a31b8657d13127799f26461f862c8
Muver <via.zyn@ya.ru>
2021-01-25 23:57:21 +0300

    android ui finished

--- Android/app/src/main/java/com/muver/chars/MainActivity.java
+++ Android/app/src/main/java/com/muver/chars/MainActivity.java
@@ -2,13 +2,42 @@ package com.muver.chars;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton resultButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OnClickHandler handler = new OnClickHandler(this);
+        resultButton = findViewById(R.id.resultButton);
+        resultButton.setOnClickListener(handler);
+    }
+}
+
+class OnClickHandler implements View.OnClickListener {
+    AppCompatActivity activity;
+    OnClickHandler(AppCompatActivity a) {
+        this.activity = a;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        activity.startActivity(shareIntent);
     }
 }
